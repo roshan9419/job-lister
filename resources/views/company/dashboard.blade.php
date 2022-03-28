6b880fa61824a8717fbfdf4c76baf4e3f434af85
Roshan <roshank9419@gmail.com>
2022-03-29 02:40:48 +0530

    company profile update + drawer responsive

--- resources/views/company/dashboard.blade.php
+++ resources/views/company/dashboard.blade.php
@@ -1,39 +1,75 @@
 <x-app-layout>
+
+  <style>
+    body {
+      background-color: #eee;
+    }
+    #side-drawer {
+      width: 280px; 
+      position: fixed;
+      padding: 10px;
+    }
+    #right-container {
+      margin-left: 280px; 
+    }
+    .tab-icon {
+      margin-right: 5px;
+    }
+    .company-logo {
+      margin-left: 5px;
+      margin-top: 5px;
+    }
+    @media (max-width: 1000px) {
+      #side-drawer {
+        width: 70px;
+      }
+      #right-container {
+        margin-left: 70px;
+      }
+      .tab-icon {
+        font-size: 16px;
+        margin-right: 0;
+      }
+      .tab-text {
+        display: none;
+      }
+    }
+  </style>
+
   <div class="container-fluid">
     <div class="row flex-nowrap">
 
-      <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark min-vh-100"
-        style="width: 280px; position: fixed;">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <div id="side-drawer" class="d-flex flex-column flex-shrink-0 bg-dark text-white min-vh-100">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none company-logo">
           <img src="{{ asset('storage/images/companies/'.$company->name_slug.'.jpg') }}" alt="" width="32" height="32"
             class="rounded-circle me-2">
-          <span class="fs-4">{{ $company->name }}</span>
+          <span class="fs-4 tab-text">{{ $company->name }}</span>
         </a>
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
           <li class="nav-item">
             <a href="?tab=profile" class="nav-link text-white {{ $tab == 'profile' ? 'active' : '' }}"
               aria-current="page">
-              {{-- <i class="fa-light fa-building"></i> --}}
-              Profile
+              <i class="bi bi-person-circle tab-icon"></i>
+              <span class="tab-text">Profile</span>
             </a>
           </li>
           <li>
             <a href="?tab=jobs-posted" class="nav-link text-white {{ $tab == 'jobs-posted' ? 'active' : '' }}">
-              {{-- <i class="fa-light fa-building"></i> --}}
-              Jobs Posted
+              <i class="bi bi-briefcase-fill tab-icon"></i>
+              <span class="tab-text">Jobs Posted</span>
             </a>
           </li>
           <li>
             <a href="?tab=create-job" class="nav-link text-white {{ $tab == 'create-job' ? 'active' : '' }}">
-              {{-- <i class="fa-light fa-building"></i> --}}
-              Create Job Post
+              <i class="bi bi-file-post tab-icon"></i>
+              <span class="tab-text">Create Job Post</span>
             </a>
           </li>
           <li>
             <a href="?tab=applications" class="nav-link text-white {{ $tab == 'applications' ? 'active' : '' }}">
-              {{-- <i class="fa-light fa-building"></i> --}}
-              Applications
+              <i class="bi bi-person-lines-fill tab-icon"></i>
+              <span class="tab-text">Applications</span>
             </a>
           </li>
         </ul>
@@ -42,20 +78,18 @@
           <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
             id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="{{ session('user')->photo_url }}" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>{{ session('user')->name }}</strong>
+            <strong class="tab-text">{{ session('user')->name }}</strong>
           </a>
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1" style="">
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+            {{-- <li><a class="dropdown-item" href="#">Profile</a></li> --}}
+            {{-- <li> <hr class="dropdown-divider"> </li> --}}
             <li><a class="dropdown-item" href="{{route('auth.logout')}}">Sign out</a></li>
           </ul>
         </div>
 
       </div>
 
-      <div class="col py-3" style="margin-left: 280px; background-color: #eee;">
+      <div id="right-container" class="col py-3">
         @switch($tab)
           @case('profile')
             @include('company.profile')
--- resources/views/company/profile.blade.php
+++ resources/views/company/profile.blade.php
@@ -1,147 +1,56 @@
-    <div class="container py-5">
-  
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="card mb-4">
-            <div class="card-body text-center">
-              <img src="{{ asset('storage/images/companies/'.$company->name_slug.'.jpg') }}" alt="avatar" class="img-fluid" style="width: 150px">
-              <h5 class="my-3">{{ $company->name }}</h5>
-              <p class="text-muted mb-1">{{$company->about}}</p>
-              <p class="text-muted mb-4">{{$company->state}}, {{$company->country}}</p>
-            </div>
+<style>
+  .cover {
+    background-image: url(https://images.unsplash.com/photo-1530305408560-82d13781b33a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80);
+    background-size: cover;
+    background-repeat: no-repeat
+  }
+
+  .icon {
+    font-size: 1.3rem;
+  }
+</style>
+
+<div class="row py-5">
+  <div class="col-md-10 mx-auto">
+    <div class="bg-white shadow rounded overflow-hidden">
+      <div class="px-4 pt-0 cover">
+        <div class="media align-items-end pt-5 row">
+          <div class="profile mr-3">
+            <img src="{{ asset('storage/images/companies/'.$company->name_slug.'.jpg') }}" alt="..." width="130"
+              class="rounded mb-2">
           </div>
-          <div class="card mb-4 mb-lg-0">
-            <div class="card-body p-0">
-              <ul class="list-group list-group-flush rounded-3">
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fas fa-globe fa-lg text-warning"></i>
-                  <a class="mb-0" href="{{$company->website}}" style="text-decoration: none">{{$company->website}}</a>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-github fa-lg" style="color: #333333;"></i>
-                  <p class="mb-0">mdbootstrap</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-twitter fa-lg" style="color: #55acee;"></i>
-                  <p class="mb-0">@mdbootstrap</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
-                  <p class="mb-0">mdbootstrap</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
-                  <p class="mb-0">mdbootstrap</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-8">
-          <div class="card mb-4">
-            <div class="card-body">
-              <div class="row">
-                <div class="col-sm-3">
-                  <p class="mb-0">Full Name</p>
-                </div>
-                <div class="col-sm-9">
-                  <p class="text-muted mb-0">{{$company->name}}</p>
-                </div>
-              </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-3">
-                  <p class="mb-0">Email</p>
-                </div>
-                <div class="col-sm-9">
-                  <p class="text-muted mb-0">{{$company->email}}</p>
-                </div>
-              </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-3">
-                  <p class="mb-0">Phone</p>
-                </div>
-                <div class="col-sm-9">
-                  <p class="text-muted mb-0">(097) 234-5678</p>
-                </div>
-              </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-3">
-                  <p class="mb-0">Mobile</p>
-                </div>
-                <div class="col-sm-9">
-                  <p class="text-muted mb-0">(098) 765-4321</p>
-                </div>
-              </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-3">
-                  <p class="mb-0">Headquarter</p>
-                </div>
-                <div class="col-sm-9">
-                  <p class="text-muted mb-0">{{$company->state}}, {{$company->country}}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="card mb-4 mb-md-0">
-                <div class="card-body">
-                  <p class="mb-4"><span class="text-primary font-italic me-1">assigment</span> Project Status</p>
-                  <p class="mb-1" style="font-size: .77rem;">Web Design</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                  <div class="progress rounded mb-2" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="card mb-4 mb-md-0">
-                <div class="card-body">
-                  <p class="mb-4"><span class="text-primary font-italic me-1">assigment</span> Project Status</p>
-                  <p class="mb-1" style="font-size: .77rem;">Web Design</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                  <div class="progress rounded" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                  <div class="progress rounded mb-2" style="height: 5px;">
-                    <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="text-white">
+            <h4 class="mt-0 mb-0">{{ $company->name }}</h4>
+            <p class="small"> <i class="bi bi-geo-alt"></i> {{ $company->state }}, {{
+              $company->country }}</p>
           </div>
         </div>
       </div>
+
+      <div class="bg-light px-4 py-3 d-flex justify-content-end text-center">
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">
+            <a href="{{ $company->website }}" target="_blank"><i class="bi bi-globe icon"></i></a>
+          </li>
+          <div class="list-inline-item">
+            <a href="mailto:{{ $company->email }}"><i class="bi bi-envelope icon"></i></a>
+          </div>
+        </ul>
+      </div>
+      <div class="px-4 py-3">
+        <h5 class="mb-0">About</h5>
+        <div class="p-4 rounded shadow-sm bg-light">
+          <p class="font-italic mb-0">{{ $company->about }}</p>
+        </div>
+      </div>
+      {{-- <div class="py-4 px-4">
+        <div class="d-flex align-items-center justify-content-between mb-3">
+          <h5 class="mb-0">Recent jobs</h5><a href="{{ route('jobs.list', ['company' => $company->company_id]) }}"
+            class="btn btn-link text-muted">View all</a>
+        </div>
+        <div class="row">
+        </div>
+      </div> --}}
     </div>
+  </div>
+</div>
